183e797d43c8c16182202b0d4ff4b40b6ceb4816
ThiagoRAlmeida2 <thiagoralmeida23@gmail.com>
2024-09-17 09:02:43 -0300

    Criando router

--- src/html/Componentes/header.php
+++ src/html/Componentes/header.php
@@ -5,11 +5,11 @@
 
     <nav class="navBar">
         <ul class="navBarList">
-            <a href="/html/pages/doe.php"><li>Realizar uma doação</li></a>
-            <a href="/html/pages/adote.php"><li>Adotar Pet</li></a>
-            <a href="/html/pages/apadrinhe.php"><li>Apadrinhe</li></a>
-            <a href="/html/pages/resgates.php"><li>Resgates</li></a>
-            <a href="/html/pages/quemSomos.php"><li>Quem somos</li></a>
+            <a href="../html/Pages/doe.php"><li>Realizar uma doação</li></a>
+            <a href="../html/Pages/adote.php"><li>Adotar Pet</li></a>
+            <a href="../html/Pages/apadrinhe.php"><li>Apadrinhe</li></a>
+            <a href="../html/Pages/resgates.php"><li>Resgates</li></a>
+            <a href="../html/Pages/quemSomos.php"><li>Quem somos</li></a>
         </ul>
     </nav>
 </header>
