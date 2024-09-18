89c0c109f55e10ba982f31030b9b73a2db5ef922
ThiagoRAlmeida2 <thiagoralmeida23@gmail.com>
2024-09-18 20:39:41 -0300

    Adicionando slides

--- src/home.php
+++ src/home.php
@@ -9,6 +9,8 @@ include 'Componentes/header.php';
         <h1>Bem-vindo ao Projeto Recomeços</h1>
         <p>Descrição ou conteúdo principal da página.</p>
     </div>
+    <?php include 'Componentes/slides.php' ?>
 </main>
 <?php include 'Componentes/footer.php'; ?>
 <script src="/JavaScript/index.js"></script>
+<script src="/JavaScript/slides.js"></script>
