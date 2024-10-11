a02d01050ee8066155f2d00691850c9d85e57ed1
ThiagoRAlmeida2 <thiagoralmeida23@gmail.com>
2024-10-11 08:35:15 -0300

    salvando alteraçoes feitas

--- src/adicionarPet.php
+++ src/adicionarPet.php
@@ -1,5 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
+<?php 
+include "Componentes/header.php"
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- src/quemSomos.php
+++ src/quemSomos.php
@@ -7,17 +7,39 @@ include 'Componentes/header.php';
     <title><?php echo $titulo; ?></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../Css/style.css">
+    <link rel="stylesheet" href="/css/quemSomos.css">
+    <style>
+        
+    </style>
 </head>
 
 <body>
-
     <div>
-        <h1>Quem somos nós?</h1>
-        <p>Descrição ou conteúdo principal da página.</p>
+        <h1>QUEM SOMOS</h1>
+        <div class="team-container">
+            <div class="team-member">
+            <img src="assets/images/transferir.png" alt="Amanda">
+                <h3>Amanda</h3>
+                <p>Enfermeira</p>
+            </div>
+            <div class="team-member">
+                <img src="https://via.placeholder.com/150" alt="Ana Mariano">
+                <h3>Ana Mariano</h3>
+                <p>Produtora</p>
+            </div>
+            <div class="team-member">
+                <img src="https://via.placeholder.com/150" alt="Bruna Eiras">
+                <h3>Bruna Eiras</h3>
+                <p>Enfermeira</p>
+            </div>
+            <div class="team-member">
+                <img src="https://via.placeholder.com/150" alt="Bruna Hernandez">
+                <h3>Bruna Hernandez</h3>
+                <p>Enfermeira</p>
+            </div>
+            <!-- Adicione mais membros aqui -->
+        </div>
     </div>
-
-
 </body>
 
 <?php include 'Componentes/footer.php'; ?>
