1a0c0bd9bdde30b5961b2686cf0ed82df8ba16de
Marcelo Santiago <marcelo.sant87@outlook.com>
2024-10-16 17:35:16 -0300

    Update home.php
    First sketch of the home page.

--- src/home.php
+++ src/home.php
@@ -6,8 +6,38 @@ include 'Componentes/header.php';
 
 <main>
     <div>
+        <!-- PRIMEIRO BLOCO - FOTO 'NOSSO LAR' -->
+        <!-- Foto do local com uma descricao dizendo Nosso Lar -->
         <h1>Bem-vindo ao Projeto Recomeços</h1>
         <p>Descrição ou conteúdo principal da página.</p>
+    </div>
+    <div>
+        <!-- SEGUNDO BLOCO - HISTORIA DO PROJETO -->
+        <!-- Descricao da historia do projeto deles -->
+        <h2>A historia do projeto</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <img src="imagem_grupo">
+    </div>
+    <div>
+        <!-- TERCEIRO BLOCO - TRES IMAGENS -->
+        <!-- Serao tres imagens, com tres botoes em cada img --> 
+        <img src="imagem_grupo">
+        <p>Quero apadrinhar!</p>
+        <img src="imagem_do_dog">
+        <p>Fazer uma doacao</p>
+        <img src="imagem_moca_tres_dogs">
+        <p>Quero adotar!</p>
+    </div>
+    <div>
+        <!-- QUARTO BLOCO - CICLO DE RESGATE -->
+        <h2>Ciclo de resgate</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div>
+        <!-- QUINTO BLOCO - CASE DE SUCESSO com imagem -->
+        <img src="imagem_case_sucesso">
+        <h2>Case de sucesso</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
     <?php include 'Componentes/slides.php' ?>
 </main>
