d57c750debc44cbe3e0ba3bd01deee7756d3380c
Marcelo Santiago <marcelo.sant87@outlook.com>
2024-10-29 22:55:23 -0300

    Update home.php
    Update no html e no css da home, adicionando imagens temporarias, alinhando elementos e alterando suas cores.

--- src/home.php
+++ src/home.php
@@ -7,43 +7,59 @@ include 'Componentes/header.php';
     <title><?php echo $titulo; ?></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/home.css">
+    <link   
+ href="https://fonts.googleapis.com/css2?family=Fredoka:wght@400&display=swap" rel="stylesheet">
 </head>
 <main>
     <div>
         <!-- PRIMEIRO BLOCO - FOTO 'NOSSO LAR' -->
         <!-- Foto do local com uma descricao dizendo Nosso Lar -->
-        <h1>Bem-vindo ao Projeto Recomeços</h1>
-        <p>Descrição ou conteúdo principal da página.</p>
+        <img src="assets/images/dog_aleatorio.jpg">
+        <h1>Nosso abrigo</h1>
     </div>
-    <div>
+
+    <div class="container">
         <!-- SEGUNDO BLOCO - HISTORIA DO PROJETO -->
         <!-- Descricao da historia do projeto deles -->
-        <h2>A historia do projeto</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <img src="imagem_grupo">
+         <div class="texto-direita">
+            <h2 class="texto-titulo">A historia do projeto</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+         </div>
+        <img class="img-esquerda" src="assets/images/dog_aleatorio.jpg">
     </div>
-    <div>
+
+    <div class="container">
         <!-- TERCEIRO BLOCO - TRES IMAGENS -->
         <!-- Serao tres imagens, com tres botoes em cada img --> 
-        <img src="imagem_grupo">
-        <p>Quero apadrinhar!</p>
-        <img src="imagem_do_dog">
-        <p>Fazer uma doacao</p>
-        <img src="imagem_moca_tres_dogs">
-        <p>Quero adotar!</p>
+        <div class="image-container">
+            <img src="assets/images/dog_aleatorio.jpg">
+            <button class="button" id="button-overlay">Quero apadrinhar!</button>
+        </div>
+        <div class="image-container">
+            <img src="assets/images/dog_aleatorio.jpg">
+            <button class="button" id="button-overlay">Fazer doacao</button> 
+        </div>
+        <div class="image-container">
+            <img src="assets/images/dog_aleatorio.jpg">
+            <button class="button" id="button-overlay">Quero adotar!</button>
+        </div>
     </div>
-    <div>
-        <!-- QUARTO BLOCO - CICLO DE RESGATE -->
-        <h2>Ciclo de resgate</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-    <div>
-        <!-- QUINTO BLOCO - CASE DE SUCESSO com imagem -->
-        <img src="imagem_case_sucesso">
-        <h2>Case de sucesso</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
+    <div id="ciclo-resgate">
+        <!-- QUARTO BLOCO - CICLO DE RESGATE -->
+        <h2 class="texto-centralizado">Ciclo de resgate</h2>
+        <p class="texto-centralizado">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+
+    <div class="container">
+        <!-- QUINTO BLOCO - CASE DE SUCESSO com imagem -->
+        <img class="img-direita" src="assets/images/dog_aleatorio.jpg">
+        <div class="texto-esquerda">
+            <h2 class="texto-titulo">Case de sucesso</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+         </div>  
+    </div>
 </main>
 
 <?php include 'Componentes/footer.php'; ?>
