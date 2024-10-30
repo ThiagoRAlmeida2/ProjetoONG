c33fcb5228e047c57b1d637989e1262efb766212
ThiagoRAlmeida2 <thiagoralmeida23@gmail.com>
2024-10-30 11:02:59 -0300

    Revert "excluindo arquivos"
    This reverts commit 4221b1a9a6eab1402c75d47073ce07f294fac35c.

---
+++ src/resgates.php
@@ -0,0 +1,31 @@
+<?php
+$titulo = "Animais resgatados";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <title><?php echo $titulo; ?></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="../../Css/header.css">
+        <link rel="stylesheet" href="../../Css/style.css">
+        <link rel="stylesheet" href="../../Css/footer.css">
+    </head>
+
+    <body>
+
+        <?php include '../Componentes/header.php'; ?>
+
+        <div>
+            <h1>Animais resgatados!</h1>
+            <p>Descrição ou conteúdo principal da página.</p>
+        </div>
+
+        <?php include '../Componentes/footer.php'; ?>
+
+    </body>
+
+    <script src="../../JavaScript/index.js"></script>
+</html>
